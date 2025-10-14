f0ff82a45403181d20e34c18ea236769faf3f680
czsdesigninfo <czsdesigninfo@5hd7cx.onmicrosoft.com>
2025-10-14 19:23:58 -0300

    deleted:    Fundamentos/exercicio_3-4.py 	deleted:    Fundamentos/exercicio_3-5.py 	deleted:    Fundamentos/exercicio_3-6.py 	deleted:    Fundamentos/fundamentos.py

--- Fundamentos/exercicio_3-4.py
+++
@@ -1,6 +0,0 @@
-# Escreva uma expressão para determinar se uma pessoa deve ou não pagar imposto.
-# Considere que pagam imposto pessoas cujo salário é maior que R$1.200,00.
-
-salario = 800.0
-
-print(salario > 1200.0)
